a35ce1c55e9f25efb3508961957dd1a9a98e1c90
Konrad <29203289+konivik@users.noreply.github.com>
2020-03-19 20:59:12 +0100

    Change ScenarioRunner to EvalRunner

--- src/org/emoflon/flight/cep/test/FlightCEPMonitorDemo.java
+++ src/org/emoflon/flight/cep/test/FlightCEPMonitorDemo.java
@@ -1,16 +1,17 @@
 package org.emoflon.flight.cep.test;
 
 import org.emoflon.flight.cep.util.FlightApamaMonitor;
-import org.emoflon.flight.scenario.ScenarioRunner;
+import org.emoflon.flight.scenario.EvalaluationScenarioRunner;
 
 import flight.monitor.FlightMonitor;
 
 public class FlightCEPMonitorDemo {
 	
 	public static void main(String[] args) {
-		ScenarioRunner runner = new ScenarioRunner();
+		EvalaluationScenarioRunner runner = new EvalaluationScenarioRunner(5);
 		runner.initModel("../Flights/src/org/emoflon/flight/model/definitions");
 		runner.initModelEventGenerator(15, 12, 51, 0.01, 0.5);
+		runner.addFlightsAndBookings(4);
 		
 		FlightMonitor monitor = new FlightApamaMonitor();
 		monitor.initModelAndEngine(runner.getModel());
