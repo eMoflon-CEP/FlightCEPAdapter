c46215e72678c70ac1dcc85618d30ff0467f8fad
SebastianE <sebastian.ehmes@gmail.com>
2020-05-06 15:51:40 +0200

    correlator is displaying shell messages on the java console connecting the engine client interface works

--- src/org/emoflon/flight/cep/test/ApamaAPITest.java
+++ src/org/emoflon/flight/cep/test/ApamaAPITest.java
@@ -1,8 +1,14 @@
 package org.emoflon.flight.cep.test;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
-import com.apama.epl.plugin.Correlator;
+import com.apama.EngineException;
+import com.apama.engine.EngineStatus;
+import com.apama.engine.beans.EngineClientFactory;
+import com.apama.engine.beans.interfaces.EngineClientInterface;
+
 
 public class ApamaAPITest {
 
@@ -12,12 +18,21 @@ public class ApamaAPITest {
 		
 		tr.start();
 		
-		try {
-			Thread.sleep(5000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		EngineClientInterface engineClient = initClient("localhost", 15903, "ApamaAPITest");
+		if(engineClient != null) {
+			try {
+				engineClient.connectNow();
+				EngineStatus status = engineClient.getRemoteStatus();
+				System.out.println(status);
+			} catch (EngineException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+		}else {
+			System.out.println("Could not initialize EngineClientInterface.");
 		}
+			
+		
 		
 		tr.close();
 		
@@ -29,11 +44,23 @@ public class ApamaAPITest {
 		}
 		
 	}
+	
+	public static EngineClientInterface initClient(String host, int port, String pid) {
+		try {
+			return EngineClientFactory.createEngineClient(host, port, pid);
+		} catch (EngineException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		}
+	}
 }
+
 
 class ProcessThread extends Thread {
 	private Process pr = null;
 	private String execPath;
+	private boolean running = true;
 	
 	public ProcessThread(String execPath) {
 		this.execPath = execPath;
@@ -45,6 +72,27 @@ class ProcessThread extends Thread {
 		{
 			pr = runtime.exec(execPath);        //opens new notepad instance
 			System.out.println("Running: " + pr.toString()+" status: "+pr.isAlive());
+			
+			BufferedReader stdInput = new BufferedReader(new 
+				     InputStreamReader(pr.getInputStream()));
+
+				BufferedReader stdError = new BufferedReader(new 
+				     InputStreamReader(pr.getErrorStream()));
+				while(running) {
+					// Read the output from the command
+					System.out.println("***APAMA-PROCRESS Output:\n");
+					String s = null;
+					while ((s = stdInput.readLine()) != null) {
+					    System.out.println(s);
+					}
+
+					// Read any errors from the attempted command
+					System.out.println("***APAMA-PROCRESS Errors:\n");
+					while ((s = stdError.readLine()) != null) {
+					    System.out.println(s);
+					}
+				}
+				
 		}
 		catch (IOException e)
 		{
@@ -56,5 +104,6 @@ class ProcessThread extends Thread {
 		System.out.println("Killing: "+pr.toString()+" status: "+pr.isAlive());
 		pr.destroy();
 		System.out.println("Exited with: "+pr.exitValue());
+		running = false;
 	}
 }
